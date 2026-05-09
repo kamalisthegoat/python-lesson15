69c004cb16defd063149cbaada77ecbab4d3d7b2
kamalisthegoat <manjusundaram1983@gmail.com>
2026-05-09 20:27:06 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,9 @@
+a = input("enter a word (in caps) :")
+
+for i in a:
+    if i == 'A':
+        print("A is found")
+        break
+
+    else:
+        print("A is not found")
---
+++ activity2.py
@@ -0,0 +1,16 @@
+
+for y in range(21):
+    if y % 20 == 0:
+        print("twist")
+
+    elif y % 15 == 0:
+        pass
+
+    elif y % 5 == 0:
+        print("fizz")
+
+    elif y % 3 == 0:
+        print("buzz")
+
+    else:
+        print(y)             
